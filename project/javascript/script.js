173854527cbbbeed8fcbde27ce83a99413c01f3e
Lentswe215 <ephraimmamonong@gmail.com>
2020-07-19 20:02:24 +0200

    changed my navbar

--- project/javascript/script.js
+++ project/javascript/script.js
@@ -1,12 +1,30 @@
 window.sr = ScrollReveal();
-sr.reveal('.anim')
+sr.reveal('.anim');
 
+const menuBtn = document.querySelector('.menu-bars');
+const menu = document.querySelector('.menu');
+const navList = document.querySelector('.nav-list');
+const navItems = document.querySelectorAll('.nav-item');
+let showMenu = false;
 
-$(document).ready(() => {
-    $('#menu-bars').click(() => {
-        $('ul').toggleClass('open');
-    })
-    $('body').on('click', 'nav a', () => {
-        $('ul').toggleClass('open');
-    })
-})
+menuBtn.addEventListener('click', () => {
+	if (!showMenu) {
+		menuBtn.classList.add('close');
+		menu.classList.add('show');
+		navList.classList.add('show');
+		navItems.forEach((item) => {
+			item.classList.add('show');
+		});
+
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove('close');
+		menu.classList.remove('show');
+		navList.classList.remove('show');
+		navItems.forEach((item) => {
+			item.classList.remove('show');
+		});
+
+		showMenu = false;
+	}
+});
