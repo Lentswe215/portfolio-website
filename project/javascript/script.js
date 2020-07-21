7a6a77313bb280a2671a671fa38ed27bb0cdf412
Lentswe215 <ephraimmamonong@gmail.com>
2020-07-21 23:49:18 +0200

    fixed errors

--- project/javascript/script.js
+++ project/javascript/script.js
@@ -7,6 +7,7 @@ const navList = document.querySelector('.nav-list');
 const navItems = document.querySelectorAll('.nav-item');
 let showMenu = false;
 
+console.log('i am visible')
 menuBtn.addEventListener('click', (e) => {
 
 	if (!showMenu) {
