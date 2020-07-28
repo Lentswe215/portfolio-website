b2dd776a8639994feeb9bce67fb14bb5ee4b53ea
Lentswe215 <ephraimmamonong@gmail.com>
2020-07-28 18:46:14 +0200

    copied index file to host the websites

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+sr.reveal('.anim');
+
+const menuBtn = document.querySelector('.menu-bars');
+const menu = document.querySelector('.menu');
+const navList = document.querySelector('.nav-list');
+const navItems = document.querySelectorAll('.nav-item');
+let showMenu = false;
+
+console.log('i am visible')
+menuBtn.addEventListener('click', (e) => {
+
+	if (!showMenu) {
+		menuBtn.classList.add('close');
+		menu.classList.add('show');
+		navList.classList.add('show');
+		navItems.forEach((item) => {
+			item.classList.add('show');
+		});
+
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove('close');
+		menu.classList.remove('show');
+		navList.classList.remove('show');
+		navItems.forEach((item) => {
+			item.classList.remove('show');
+		});
+
+		showMenu = false;
+	}
+});
